5a197da8dd02d9b1800fd09ade7c0054b9d0b52e
Mads Eckardt <mads.eckardt@gmail.com>
2022-11-04 11:38:28 +0100

    fix

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
-SCRIPT=$(readlink -f "$0")
+SCRIPT=$(readlink -f "${BASH_SOURCE[0]}")
 SCRIPTDIR=$(dirname $SCRIPT)
-ln -sv $SCRIPTDIR "$HOME/.config/kak"
+ln -sv $SCRIPTDIR "$HOME/.config/kak/"
